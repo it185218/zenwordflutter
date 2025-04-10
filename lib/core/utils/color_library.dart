125573b875d68889dda075fcc456ac60662afc02
Darynne Keith Asis <adarynnekeith@gmail.com>
2025-04-11 00:50:18 +0800

    add color library

---
+++ lib/core/utils/color_library.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class ColorLibrary {
+  // 85 = 217 , 90 = 230, 75 = 191
+
+  static const Color button = Color.fromARGB(217, 61, 44, 30);
+  static const Color buttonBorder = Color.fromARGB(255, 212, 163, 115);
+  static const Color buttonText = Color.fromARGB(255, 245, 235, 221);
+
+  static const Color coinContainer = Color.fromARGB(191, 30, 43, 56);
+  static const Color coinText = Color.fromARGB(255, 255, 215, 0);
+  static const Color coin = Color.fromARGB(255, 244, 197, 66);
+
+  static const Color roundContainer = Color.fromARGB(230, 66, 47, 29);
+  static const Color letter = Color.fromARGB(255, 245, 235, 221);
+  static const Color letterSelected = Color.fromARGB(255, 66, 47, 29);
+  static const Color letterContainer = Color.fromARGB(255, 245, 235, 221);
+  static const Color gridEmpty = Color.fromARGB(255, 245, 235, 221);
+}
