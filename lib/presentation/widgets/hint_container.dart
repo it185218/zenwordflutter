41763e4e7140300c51d37723b851d6ef9e2fdca9
Darynne Keith Asis <adarynnekeith@gmail.com>
2025-04-11 22:21:32 +0800

    animate button

--- lib/presentation/widgets/hint_container.dart
+++ lib/presentation/widgets/hint_container.dart
@@ -1,22 +1,57 @@
 import 'package:flutter/material.dart';
-
 import '../../core/utils/color_library.dart';
+import '../../core/utils/scale_animation_helper.dart';
 
-class HintContainer extends StatelessWidget {
+class HintContainer extends StatefulWidget {
   final IconData icon;
+  final VoidCallback? onTap;
 
-  const HintContainer({super.key, required this.icon});
+  const HintContainer({super.key, required this.icon, this.onTap});
+
+  @override
+  State<HintContainer> createState() => _HintContainerState();
+}
+
+class _HintContainerState extends State<HintContainer> {
+  double _scale = 1.0;
+
+  void _handleTap() {
+    widget.onTap?.call();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 60,
-      height: 60,
-      decoration: BoxDecoration(
-        color: ColorLibrary.button,
-        shape: BoxShape.circle,
+    return GestureDetector(
+      onTapDown: (details) {
+        AnimationHelper.onTapDown(details, (newScale) {
+          setState(() => _scale = newScale);
+        });
+      },
+      onTapUp: (details) {
+        AnimationHelper.onTapUp(details, (newScale) {
+          setState(() => _scale = newScale);
+        });
+        _handleTap();
+      },
+      onTapCancel: () {
+        AnimationHelper.onTapCancel((newScale) {
+          setState(() => _scale = newScale);
+        });
+      },
+      child: AnimatedScale(
+        scale: _scale,
+        duration: const Duration(milliseconds: 100),
+        curve: Curves.easeInOut,
+        child: Container(
+          width: 60,
+          height: 60,
+          decoration: BoxDecoration(
+            color: ColorLibrary.button,
+            shape: BoxShape.circle,
+          ),
+          child: Icon(widget.icon, color: Colors.white, size: 30),
+        ),
       ),
-      child: Icon(icon, color: Colors.white, size: 30),
     );
   }
 }
