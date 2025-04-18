3f89023918e3f9272dd0a742c6f331fe4c4d2ad7
Darynne Keith Asis <adarynnekeith@gmail.com>
2025-04-18 11:37:32 +0800

    change color palette

--- lib/core/utils/color_library.dart
+++ lib/core/utils/color_library.dart
@@ -3,33 +3,33 @@ import 'package:flutter/material.dart';
 class ColorLibrary {
   // 85 = 217 , 90 = 230, 75 = 191
 
-  static const Color background = Color.fromARGB(250, 108, 182, 178);
+  static const Color background = Color.fromARGB(255, 117, 199, 197);
   static const Color backgroundOverlay = Color.fromARGB(25, 108, 182, 178);
 
-  static const Color button = Color.fromARGB(125, 209, 191, 146);
-  static const Color buttonBorder = Color.fromARGB(255, 217, 187, 124);
+  static const Color button = Color.fromARGB(64, 203, 182, 133);
+  static const Color buttonBorder = Color.fromARGB(255, 214, 182, 115);
   static const Color buttonText = Color.fromARGB(255, 255, 255, 255);
 
-  static const Color coinContainer = Color.fromARGB(200, 108, 182, 178);
-  static const Color coinContainerBorder = Color.fromARGB(250, 161, 211, 193);
-  static const Color coinText = Color.fromARGB(255, 255, 255, 255);
-  static const Color coinFill = Color.fromARGB(255, 244, 218, 142);
-  static const Color coinFillBorder = Color.fromARGB(255, 250, 240, 203);
+  static const Color coinContainer = Color.fromARGB(204, 93, 163, 159);
+  static const Color coinContainerBorder = Color.fromARGB(255, 150, 198, 181);
+  static const Color coinText = Color.fromARGB(255, 253, 253, 253);
+  static const Color coinFill = Color.fromARGB(255, 239, 214, 132);
+  static const Color coinFillBorder = Color.fromARGB(255, 255, 246, 217);
 
   static const Color roundContainer = Color.fromARGB(150, 163, 130, 89);
   static const Color letter = Color.fromARGB(255, 255, 255, 255);
   static const Color letterSelected = Color.fromARGB(255, 255, 255, 255);
-  static const Color letterContainer = Color.fromARGB(255, 107, 203, 199);
-  static const Color gridEmpty = Color.fromARGB(255, 239, 225, 194);
-  static const Color gridFilled = Color.fromARGB(255, 163, 130, 89);
+  static const Color letterContainer = Color.fromARGB(255, 117, 199, 197);
+  static const Color gridEmpty = Color.fromARGB(255, 232, 220, 186);
+  static const Color gridFilled = Color.fromARGB(255, 156, 122, 80);
 
   static const Color dialogContainer = Color.fromARGB(255, 226, 240, 240);
   static const Color dialogContainer1 = Color.fromARGB(255, 211, 233, 232);
   static const Color dialogContainer2 = Color.fromARGB(255, 196, 226, 224);
   static const Color dialogContainer3 = Color.fromARGB(255, 182, 219, 217);
 
-  static const Color progress = Color.fromARGB(250, 108, 182, 178);
-  static const Color progressBackground = Color.fromARGB(250, 145, 155, 154);
+  static const Color progress = Color.fromARGB(255, 117, 199, 197);
+  static const Color progressBackground = Color.fromARGB(255, 145, 155, 154);
 
   static const Color dialogText = Color.fromARGB(255, 66, 70, 70);
 }
